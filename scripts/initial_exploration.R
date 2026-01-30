b07bdedeb8778d798376d1d5460ae38e0cf1f2c8
tori-sayles <rmb23nvu@uea.ac.uk>
2026-01-30 14:39:43 +0000

    uploaded data looked through and skimmed to identify isssues in the data set

---
+++ scripts/initial_exploration.R
@@ -0,0 +1,42 @@
+# Week 1: Initial Data Exploration ====
+# Author: [tori sayles]
+# Date: [30.02.26]
+
+# Load packages ====
+library('tidyverse')
+library('here')
+library('naniar')
+library('janitor')
+library('skimr')
+# Load data ====
+mosquito_egg_raw <- read_csv(here("data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this? insect?
+#   
+# - What's being measured? the mass of female mosquitos compared to their age 
+#   
+# - How many observations?205
+#   
+# - Anything surprising? minimum body mass is -93 mg
+#   
+# - Any obvious problems?a few NA values is various columns there are 13 missing values in the collector column 
+#
